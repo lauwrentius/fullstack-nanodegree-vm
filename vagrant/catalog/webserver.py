76fd2f3518c3e7f2eeb4e9e35d5d61c0dc6d6cd7
Lauwrentius Margetan <lauwrentius@gmail.com>
2017-10-23 06:02:50 -0700

    commit from home

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -14,6 +14,7 @@ import requests
 
 import pprint
 
+
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
@@ -25,6 +26,7 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 
 # @app.route('/restaurants/<int:restaurant_id>/menu/JSON')
@@ -46,8 +48,88 @@ def showLogin():
 
     test = pprint.pformat(login_session)
 
-    return render_template('login.html.j2', STATE=state, test=test)
+    return render_template('login.html.j2', state=state, test=login_session)
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
+
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -238,51 +320,7 @@ def deleteItem(item_id):
     session.commit()
 
     return redirect(url_for('displayItems'))
-# @app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
-# def newMenuItem(restaurant_id):
-#
-#     if request.method == 'POST':
-#         newItem = MenuItem(name=request.form['name'], description=request.form[
-#                            'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
-#         session.add(newItem)
-#         session.commit()
-#         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
-#     else:
-#         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
-#
-#
-# @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
-#            methods=['GET', 'POST'])
-# def editMenuItem(restaurant_id, menu_id):
-#     editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
-#     if request.method == 'POST':
-#         if request.form['name']:
-#             editedItem.name = request.form['name']
-#         if request.form['description']:
-#             editedItem.description = request.form['name']
-#         if request.form['price']:
-#             editedItem.price = request.form['price']
-#         if request.form['course']:
-#             editedItem.course = request.form['course']
-#         session.add(editedItem)
-#         session.commit()
-#         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
-#     else:
-#
-#         return render_template(
-#             'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
-#
-#
-# @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete',
-#            methods=['GET', 'POST'])
-# def deleteMenuItem(restaurant_id, menu_id):
-#     itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
-#     if request.method == 'POST':
-#         session.delete(itemToDelete)
-#         session.commit()
-#         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
-#     else:
-#         return render_template('deleteconfirmation.html', item=itemToDelete)
+
 
 
 if __name__ == '__main__':
